6c4626529764cad10cee2e041cc3203150babfea
DAVID KUKHARCHUK <god.of.cplpl@gmail.com>
2021-09-07 15:34:26 +0300

    registrationPanel-v-0.4

--- src/pages/Chekout/Chekout.tsx
+++ src/pages/Chekout/Chekout.tsx
@@ -8,8 +8,8 @@ export const Chekout = (): JSX.Element => {
     const [name, setName] = useState('')
     const [phone, setPhone] = useState('')
     const [comment, setСomment] = useState('')
-    const [checked, setChecked] = useState(false);
-  
+    const [isChecked, setChecked] = useState(false)
+
     return (
         <div className={"component"}>
             <IonHeader style={{height: 72}} className={'flex-row flex-center'}>
@@ -21,31 +21,31 @@ export const Chekout = (): JSX.Element => {
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
-            <div className={'wrapperChekout'}>
-                <IonItem disabled={!checked} lines="none" color='light' style={{with: '100%', height: '54px',}}>
+            <div className={'wrapper-chekout'}>
+                <IonItem lines="none" color='light' style={{with: '100%', height: '54px',}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="floating">Номер телефона*</IonLabel>
-                    <IonInput value={phone}></IonInput>
+                    <IonInput value={phone} onIonChange={e => setPhone(e.detail.value!)}></IonInput>
                 </IonItem>
-                <IonItem disabled={!checked} lines="none" color='light' style={{with: '100%', height: '54px', marginTop: '8px'}}>
+                <IonItem lines="none" color='light' style={{with: '100%', height: '54px', marginTop: '8px'}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="floating">Имя*</IonLabel>
-                    <IonInput value={name}></IonInput>
+                    <IonInput value={name} onIonChange={e => setName(e.detail.value!)}></IonInput>
                 </IonItem>
-                <IonItem disabled={!checked} lines="none" color='white' style={{with: '100%', marginTop: '16px', height: '64px'}}>
+                <IonItem lines="none" color='white' style={{with: '100%', marginTop: '16px', height: '64px'}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="stacked">+Добавить комментарий</IonLabel>
-                    <IonInput style={{height: '100px'}} value={comment}> </IonInput>
+                    <IonInput style={{height: '100px'}} value={comment} onIonChange={e => setСomment(e.detail.value!)}> </IonInput>
                 </IonItem>
                 <IonItem lines="none" style={{with: '100%', marginTop: '20px', color: 'light'}}>
-                    <div className={'checkboxText'}>Я принимаю условия пользовательского соглашения</div>
-                    <IonCheckbox checked={checked} onIonChange={e => setChecked(e.detail.checked)} slot="start" color="dark"/>
+                    <div className={'checkbox-text'}>Я принимаю условия пользовательского соглашения</div>
+                    <IonCheckbox checked={isChecked} onIonChange={e => setChecked(e.detail.checked)} slot="start" color="dark"/>
                 </IonItem>
-                <IonButton disabled={!checked} size="large" expand="block" color="dark" style={{marginTop: '30px'}}>Подтвердить</IonButton>
+                <IonButton disabled={!isChecked} size="large" expand="block" color="dark" style={{marginTop: '30px'}}>Подтвердить</IonButton>
             </div>
-            <div className={'footerBtn'}>
-                <IonButton className={'footerItemBtn footerItemBtnLeft'} size="large" expand="block">
+            <div className={'footer-btn'}>
+                <IonButton className={'footer-item-btn-left'} size="large" expand="block">
                 <IonIcon slot="start" icon={pencil}/>
                     Запись
                 </IonButton>
-                <IonButton className={'footerItemBtn footerItemBtnRight'} size="large" expand="block">
+                <IonButton className={'footer-item-btn-right'} size="large" expand="block">
                 <IonIcon slot="start" icon={user}/>
                     Личный кабинет
                 </IonButton>
