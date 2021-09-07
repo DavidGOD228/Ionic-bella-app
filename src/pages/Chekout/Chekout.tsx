06f7fbd413e2a7a806782bd130d3d61d55aa2195
DAVID KUKHARCHUK <god.of.cplpl@gmail.com>
2021-09-07 13:36:32 +0300

    registrationPanel-v-0.3

--- src/pages/Chekout/Chekout.tsx
+++ src/pages/Chekout/Chekout.tsx
@@ -1,67 +1,52 @@
 import React, {useState} from 'react'
+import user from '../../assets/icon/user_w.svg'
+import pencil from '../../assets/icon/pencil.svg'
 import './style.css'
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
-import {useSelector, useDispatch} from 'react-redux'
-
-import moment from "moment";
-import {saveDate} from '../../redux/services/actions'
-import {MainHeader} from "../../components/Header/MainHeader";
-import {IRootReducer} from "../../redux/rootReducer";
-import {useHistory} from "react-router";
-import {IonBackButton, IonButtons, IonButton, IonHeader, IonIcon, IonSearchbar, IonToolbar, IonItemDivider, IonItem, IonInput, IonLabel, IonCheckbox,} from "@ionic/react";
-import Search from "../../assets/icon/search.svg";
-import { relative } from 'path';
-import { NONAME } from 'dns';
-
-
+import {IonIcon, IonButtons, IonButton, IonHeader, IonToolbar, IonItem, IonInput, IonLabel, IonCheckbox,} from "@ionic/react";
 
 export const Chekout = (): JSX.Element => {
-    const [isShowModal, setShowModal] = useState(false)
     const [name, setName] = useState('')
     const [phone, setPhone] = useState('')
     const [comment, setСomment] = useState('')
     const [checked, setChecked] = useState(false);
-
-
-    
+  
     return (
         <div className={"component"}>
             <IonHeader style={{height: 72}} className={'flex-row flex-center'}>
                 <IonToolbar>
                     <IonButtons style={{marginLeft: 10, justifyContent: 'space-between', paddingRight: 20}}>
                         <div className={'flex-row flex-center'}>
-    ≈                            <div style={{color: 'white', paddingLeft: 20}}>{'Подтверждение записи'}</div>
+    ≈                       <div style={{color: 'white', paddingLeft: 20}}>{'Подтверждение записи'}</div>
                         </div>
                     </IonButtons>
                 </IonToolbar>
             </IonHeader>
             <div className={'wrapperChekout'}>
-                <IonItem lines="none" color='light' style={{with: '100%', height: '54px',}}>
+                <IonItem disabled={!checked} lines="none" color='light' style={{with: '100%', height: '54px',}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="floating">Номер телефона*</IonLabel>
                     <IonInput value={phone}></IonInput>
                 </IonItem>
-                <IonItem lines="none" color='light' style={{with: '100%', height: '54px', marginTop: '8px'}}>
+                <IonItem disabled={!checked} lines="none" color='light' style={{with: '100%', height: '54px', marginTop: '8px'}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="floating">Имя*</IonLabel>
                     <IonInput value={name}></IonInput>
                 </IonItem>
-                <IonItem lines="none" color='white' style={{with: '100%', marginTop: '16px', height: '64px'}}>
+                <IonItem disabled={!checked} lines="none" color='white' style={{with: '100%', marginTop: '16px', height: '64px'}}>
                     <IonLabel style={{color: 'rgba(167, 167, 171, 1)'}} position="stacked">+Добавить комментарий</IonLabel>
                     <IonInput style={{height: '100px'}} value={comment}> </IonInput>
                 </IonItem>
                 <IonItem lines="none" style={{with: '100%', marginTop: '20px', color: 'light'}}>
                     <div className={'checkboxText'}>Я принимаю условия пользовательского соглашения</div>
-                    <IonCheckbox checked={checked} slot="start" color="dark"/>
+                    <IonCheckbox checked={checked} onIonChange={e => setChecked(e.detail.checked)} slot="start" color="dark"/>
                 </IonItem>
-                <IonButton size="large" expand="block" color="dark" style={{marginTop: '30px'}}>Подтвердить</IonButton>
+                <IonButton disabled={!checked} size="large" expand="block" color="dark" style={{marginTop: '30px'}}>Подтвердить</IonButton>
             </div>
             <div className={'footerBtn'}>
                 <IonButton className={'footerItemBtn footerItemBtnLeft'} size="large" expand="block">
-                {/* <IonIcon slot="start" icon={}/> */}
+                <IonIcon slot="start" icon={pencil}/>
                     Запись
                 </IonButton>
                 <IonButton className={'footerItemBtn footerItemBtnRight'} size="large" expand="block">
-                {/* <IonIcon slot="start" icon={}/> */}
+                <IonIcon slot="start" icon={user}/>
                     Личный кабинет
                 </IonButton>
             </div>
