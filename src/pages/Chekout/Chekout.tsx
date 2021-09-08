116bef0025c5eab96a5b2da146b1ff09a0e8617a
BrotskyS <sergiypik.work@gmail.com>
2021-09-08 16:55:26 +0300

    fix eslint errors

--- src/pages/Chekout/Chekout.tsx
+++ src/pages/Chekout/Chekout.tsx
@@ -1,44 +1,44 @@
-// import React, { useState, useEffect } from 'react';
-// import { useHistory } from 'react-router';
-// import './style.css';
-// import { IonButtons, IonHeader, IonToolbar } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import './style.css';
+import { IonButtons, IonHeader, IonToolbar } from '@ionic/react';
 
-// export const Checkout = (): JSX.Element => {
-//   const [name, setName] = useState('');
-//   const [phone, setPhone] = useState('');
-//   const [comment, setComment] = useState('');
-//   const [isChecked, setChecked] = useState(false);
+export const Checkout = (): JSX.Element => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [comment, setComment] = useState('');
+  const [isChecked, setChecked] = useState(false);
 
-//   const history = useHistory();
+  const history = useHistory();
 
-//   const onRegistration = () => {
-//     history.push('/verification');
-//   };
+  const onRegistration = () => {
+    history.push('/verification');
+  };
 
-//   useEffect(() => {
-//     console.log(phone, name, comment);
-//   });
+  useEffect(() => {
+    console.log(phone, name, comment);
+  });
 
-//   return (
-//     <div className={'component'}>
-//       <IonHeader style={{ height: 72 }} className={'flex-row flex-center'}>
-//         <IonToolbar>
-//           <IonButtons style={{ marginLeft: 10, justifyContent: 'space-between', paddingRight: 20 }}>
-//             <div className={'flex-row flex-center'}>
-//               ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Подтверждение записи</div>
-//             </div>
-//           </IonButtons>
-//         </IonToolbar>
-//       </IonHeader>
-//       <div className={'wrapper-chekout'}>
-//         <form className={'chekout-form'} >
-//           <input className={'chekout-input'} value={phone} onChange={(event) => setPhone(event.target.value)} type="text" placeholder="Номер телефона*" />
-//           <input className={'chekout-input'} value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя*" />
-//           <input className={'chekout-input-comment'} value={comment} onChange={(event) => setComment(event.target.value)} type="text" placeholder="+Добавить комментарий" />
-//           <input className={'chekout-input-cheked'} defaultChecked={isChecked} onChange={() => setChecked(!isChecked)} type="checkbox"/>
-//         </form>
-//         <button className={isChecked ? 'chekout-button' : 'not-chekout-button'} onClick={() => onRegistration()} disabled={!isChecked}>Подтвердить</button>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className={'component'}>
+      <IonHeader style={{ height: 72 }} className={'flex-row flex-center'}>
+        <IonToolbar>
+          <IonButtons style={{ marginLeft: 10, justifyContent: 'space-between', paddingRight: 20 }}>
+            <div className={'flex-row flex-center'}>
+              ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Подтверждение записи</div>
+            </div>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <div className={'wrapper-chekout'}>
+        <form className={'chekout-form'} >
+          <input className={'chekout-input'} value={phone} onChange={(event) => setPhone(event.target.value)} type="text" placeholder="Номер телефона*" />
+          <input className={'chekout-input'} value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя*" />
+          <input className={'chekout-input-comment'} value={comment} onChange={(event) => setComment(event.target.value)} type="text" placeholder="+Добавить комментарий" />
+          <input className={'chekout-input-cheked'} defaultChecked={isChecked} onChange={() => setChecked(!isChecked)} type="checkbox"/>
+        </form>
+        <button className={isChecked ? 'chekout-button' : 'not-chekout-button'} onClick={() => onRegistration()} disabled={!isChecked}>Подтвердить</button>
+      </div>
+    </div>
+  );
+};
