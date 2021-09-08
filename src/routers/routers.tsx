14e95759bde3ceb8dec8cff977d77df7478828ce
BrotskyS <sergiypik.work@gmail.com>
2021-09-08 10:16:00 +0300

    fix eslint dependence

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -7,6 +7,8 @@ import { SelectServices } from '../pages/SelectServices/SelectServices';
 import { SelectDate } from '../pages/SelectDate/SelectDate';
 import { SpecialistDetail } from '../pages/SpecialistDetail/SpecialistDetail';
 import { User } from '../pages/User/User';
+import { Checkout } from '../pages/Chekout/Chekout';
+import { Verification } from '../pages/Verification/Verification';
 
 export const Routers = () => {
   return (
@@ -27,6 +29,8 @@ export const Routers = () => {
       <Route exact path="/selectDate" component={SelectDate}/>
       <Route exact path="/selectSpecialist" component={SelectSpecialist}/>
       <Route exact path="/specialistDetail" component={SpecialistDetail}/>
+      <Route exact path="/checkout" component={Checkout}/>
+      <Route exact path="/verification" component={Verification}/>
     </Switch>
   );
 };
