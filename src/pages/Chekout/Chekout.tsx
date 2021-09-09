3fdcc7b9d3800187bcef95e0a4494fc5d017f85e
DAVID KUKHARCHUK <god.of.cplpl@gmail.com>
2021-09-09 09:06:56 +0300

    aligning the input and creating a check input

--- src/pages/Chekout/Chekout.tsx
+++ src/pages/Chekout/Chekout.tsx
@@ -31,20 +31,20 @@ export const Checkout = (): JSX.Element => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <div className={'wrapper-chekout'}>
-        <form className={'chekout-form'} >
-          <input className={'chekout-input'} value={phone} onChange={(event) => setPhone(event.target.value)} type="text" placeholder="Номер телефона*" />
-          <input className={'chekout-input'} value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя*" />
-          <div className="checked-container">
-            <input className={'chekout-input-cheked'} defaultChecked={isChecked} onChange={() => setChecked(!isChecked)} type="checkbox"/>
-            <span>Я принимаю условия пользовательского соглашения.</span>
-          </div>
-          {!isShowComment &&  <span onClick={() => setShowComment(true)}>+Добавить комментарий</span>}
+      <div className={'wrapper-checkout'}>
+        <form className={'checkout-form'} >
+          <input className={'checkout-input'} value={phone} onChange={(event) => setPhone(event.target.value)} type="text" placeholder="Номер телефона*" />
+          <input className={'checkout-input'} value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя*" />
+          {!isShowComment &&  <span className={'checkout-input-comment-text'} onClick={() => setShowComment(true)}>+Добавить комментарий</span>}
           {isShowComment &&
-                <textarea className={'chekout-input-comment'} onChange={(event) => setComment(event.target.value)} name="" id="" placeholder={'Комментарий'} cols={30}></textarea>
+                <textarea className={'checkout-input-comment'} onChange={(event) => setComment(event.target.value)} name="" id="" placeholder={'Комментарий'} cols={30}></textarea>
           }
+          <label className={'checked-container'}>
+            <input className={'checkout-input-checked'} defaultChecked={isChecked} onChange={() => setChecked(!isChecked)} type="checkbox"/>
+            <span className={'checkout-input-checked-text'}>Я принимаю условия пользовательского соглашения.</span>
+          </label>
         </form>
-        <button className={isChecked ? 'chekout-button' : 'not-chekout-button'} onClick={() => onRegistration()} disabled={!isChecked}>Подтвердить</button>
+        <button className={isChecked ? 'checkout-button' : 'not-checkout-button'} onClick={() => onRegistration()} disabled={!isChecked}>Подтвердить</button>
       </div>
     </div>
   );
