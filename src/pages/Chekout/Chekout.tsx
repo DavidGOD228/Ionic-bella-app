ff1567ce613280446dab4145f2481a931465878a
DAVID KUKHARCHUK <god.of.cplpl@gmail.com>
2021-09-09 11:31:03 +0300

    update page checkout and verification

--- src/pages/Chekout/Chekout.tsx
+++ src/pages/Chekout/Chekout.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import './style.css';
-import { IonButtons, IonButton, IonHeader, IonToolbar, IonItem, IonInput, IonLabel, IonCheckbox, } from '@ionic/react';
+import { IonButtons, IonHeader, IonToolbar } from '@ionic/react';
 
 export const Checkout = (): JSX.Element => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [comment, setComment] = useState('');
   const [isChecked, setChecked] = useState(false);
+  const [isShowComment, setShowComment] = useState(false);
 
   const history = useHistory();
 
@@ -15,35 +16,35 @@ export const Checkout = (): JSX.Element => {
     history.push('/verification');
   };
 
+  useEffect(() => {
+    console.log(phone, name, comment);
+  });
+
   return (
     <div className={'component'}>
       <IonHeader style={{ height: 72 }} className={'flex-row flex-center'}>
         <IonToolbar>
           <IonButtons style={{ marginLeft: 10, justifyContent: 'space-between', paddingRight: 20 }}>
             <div className={'flex-row flex-center'}>
-    ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Подтверждение записи</div>
+              ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Подтверждение записи</div>
             </div>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <div className={'wrapper-chekout'}>
-        <IonItem lines="none" color="light" style={{ with: '100%', height: '54px', }}>
-          <IonLabel style={{ color: 'rgba(167, 167, 171, 1)' }} position="floating">Номер телефона*</IonLabel>
-          <IonInput value={phone} onIonChange={e => setPhone(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonItem lines="none" color="light" style={{ with: '100%', height: '54px', marginTop: '8px' }}>
-          <IonLabel style={{ color: 'rgba(167, 167, 171, 1)' }} position="floating">Имя*</IonLabel>
-          <IonInput value={name} onIonChange={e => setName(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonItem lines="none" color="white" style={{ with: '100%', marginTop: '16px', height: '64px' }}>
-          <IonLabel style={{ color: 'rgba(167, 167, 171, 1)' }} position="stacked">+Добавить комментарий</IonLabel>
-          <IonInput style={{ height: '100px' }} value={comment} onIonChange={e => setComment(e.detail.value!)}> </IonInput>
-        </IonItem>
-        <IonItem lines="none" style={{ with: '100%', marginTop: '20px', color: 'light' }}>
-          <div className={'checkbox-text'}>Я принимаю условия пользовательского соглашения</div>
-          <IonCheckbox checked={isChecked} onIonChange={e => setChecked(e.detail.checked)} slot="start" color="dark"/>
-        </IonItem>
-        <IonButton onClick={() => onRegistration()} disabled={!isChecked} size="large" expand="block" color="dark" style={{ marginTop: '30px' }}>Подтвердить</IonButton>
+      <div className={'wrapper-checkout'}>
+        <form className={'checkout-form'} >
+          <input className={'checkout-input'} value={phone} onChange={(event) => setPhone(event.target.value)} type="text" placeholder="Номер телефона*" />
+          <input className={'checkout-input'} value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя*" />
+          {!isShowComment && <span className={'checkout-input-comment-text'} onClick={() => setShowComment(true)}>+Добавить комментарий</span>}
+          {isShowComment &&
+            <textarea className={'checkout-input-comment'} onChange={(event) => setComment(event.target.value)} name="" id="" placeholder={'Комментарий'} cols={30}></textarea>
+          }
+          <label className={'checked-container'}>
+            <input className={'checkout-input-checked'} defaultChecked={isChecked} onChange={() => setChecked(!isChecked)} type="checkbox" />
+            <span className={'checkout-input-checked-text'}>Я принимаю условия пользовательского соглашения.</span>
+          </label>
+        </form>
+        <button className={isChecked ? 'checkout-button' : 'not-checkout-button'} onClick={() => onRegistration()} disabled={!isChecked}>Подтвердить</button>
       </div>
     </div>
   );
--- src/pages/Verification/Verification.tsx
+++ src/pages/Verification/Verification.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import user from '../../assets/icon/user_w.svg';
-import pencil from '../../assets/icon/pencil.svg';
+import { PinCode } from '../../components/PinCode/PinCode';
 import './style.css';
-import { IonIcon, IonButtons, IonButton, IonHeader, IonToolbar, IonItem, IonInput, IonLabel, } from '@ionic/react';
+import { IonButtons, IonHeader, IonToolbar } from '@ionic/react';
 
 export const Verification = (): JSX.Element => {
-  const [code, setCode] = useState('');
+  const [pinCodeValue, setPinCodeValue] = useState('');
+  const [isChecked, setChecked] = useState(false);
+
+  const handlePinCode = (text: string) => {
+    setPinCodeValue(text);
+  };
+  const onSendPinCode = () => {
+    setChecked(true);
+  };
 
   return (
     <div className={'component'}>
@@ -13,27 +20,19 @@ export const Verification = (): JSX.Element => {
         <IonToolbar>
           <IonButtons style={{ marginLeft: 10, justifyContent: 'space-between', paddingRight: 20 }}>
             <div className={'flex-row flex-center'}>
-    ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Введите код из sms</div>
+              ≈                       <div style={{ color: 'white', paddingLeft: 20 }}>Введите код из sms</div>
             </div>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
       <div className={'wrapper-verification'}>
-        <IonItem lines="none" color="light" style={{ with: '100%', height: '54px', }}>
-          <IonLabel style={{ color: 'rgba(167, 167, 171, 1)' }} position="floating">Номер телефона*</IonLabel>
-          <IonInput value={code} onIonChange={e => setCode(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonButton disabled={false} size="large" expand="block" color="dark" style={{ marginTop: '74px' }}>Отправить</IonButton>
-      </div>
-      <div className={'footer-btn'}>
-        <IonButton className={'footer-item-btn-left'} size="large" expand="block">
-          <IonIcon slot="start" icon={pencil}/>
-                    Запись
-        </IonButton>
-        <IonButton className={'footer-item-btn-right'} size="large" expand="block">
-          <IonIcon slot="start" icon={user}/>
-                    Личный кабинет
-        </IonButton>
+        <div className={'pin-code-wrapper, pin-code-center'}>
+          <div>
+            <PinCode value={pinCodeValue} onChange={handlePinCode} onComplete={onSendPinCode} />
+            <span className={'send-again'}>Повторить отправку кода</span>
+          </div>
+        </div>
+        <button className={isChecked ? 'checkout-button' : 'not-checkout-button'} disabled={!isChecked} style={{ marginTop: 52 }}>отправить</button>
       </div>
     </div>
   );
